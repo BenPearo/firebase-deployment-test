f0f0aa37164f12e1c789001fb6a2e8f5283181bc
benpearo <benpearo@gmail.com>
2019-07-27 00:35:54 -0400

    finished function example

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// backend can bypass security roles
+admin.initializeApp(functions.config().firebase);
+
+
+// creates a document.create triggered function
+// adds a message field from name to new document
+exports.sendMessage = functions.firestore
+    .document('products/{productId}')
+    .onCreate((snapshot, context) => {
+ 
+        // gets data from snapshot
+        const newProduct = snapshot.data();
+ 
+        // gets name from data
+        const name = newProduct.name;
+
+        // gets id of changed product from context
+        const docId = context.params.productId;
+
+        const productRef = admin.firestore().collection('products').doc(docId);
+
+        return productRef.update({ message: `Nice ${name}! - Love Cloud Functions`});
+    });
